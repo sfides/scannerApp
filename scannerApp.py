5900861fd00e266f0be8e7ba60d6eee9df7a1285
sfides <cmrn930@gmail.com>
2025-11-24 00:07:48 +1300

    idk

--- scannerApp.py
+++ scannerApp.py
@@ -1,1 +1,15 @@
-print("hello")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/classify", methods=["POST"])
+def classify():
+    item_type = request.form.get("item_type")
+    return f"You selected: {item_type}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
